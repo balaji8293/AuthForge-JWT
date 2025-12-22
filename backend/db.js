7357b1324172906e755c3df5bcf681f856cce7af
Mr.Balaji Sanap <59165823+balaji8293@users.noreply.github.com>
2025-12-22 22:45:16 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,26 +1,13 @@
 const mysql = require("mysql2/promise");
 
-// Create a connection
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,        // your MySQL username
-  password: process.env.DB_PASSWORD,        // your MySQL password
-  database: process.env.DB_NAME,  // database name,
-    waitForConnections: true,
+  uri: process.env.DATABASE_URL,
+  waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectTimeout: 10000
 });
 
-// Connect to MySQL
-// connection.connect((err) => {
-//   if (err) {
-//     console.log("❌ MySQL Connection Failed:", err);
-//   } else {
-//     console.log("✅ Connected to MySQL Database!");
-//   }
-// });
-
 module.exports = db;
-
-// for start project
-// nodemon index.js
